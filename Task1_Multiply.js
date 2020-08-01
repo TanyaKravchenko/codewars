b34d94f459f5be2e6acc159c33c15054a90c525e
Tanya Kravchenko <mail-tanja@mail.ru>
2020-08-01 12:49:29 +0300

    task_1_Multiply

---
+++ Task1_Multiply.js
@@ -0,0 +1,11 @@
+//The code does not execute properly. Try to figure out why.
+//function multiply(a, b){
+//   a * b
+// }
+
+multiply = function (a, b) {
+    return a * b;
+}
+
+// the best
+//const multiply = (a, b) => a * b;
