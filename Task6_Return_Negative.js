50e0ed4788641d721cd21cd1d7bf7b847a048cf2
Tanya Kravchenko <mail-tanja@mail.ru>
2020-08-03 20:10:22 +0300

    Task6_Return_Negative

---
+++ Task6_Return_Negative.js
@@ -0,0 +1,20 @@
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+function makeNegative(num) {
+    if ( num > 0 ) {
+        return -num;
+    }
+    else {
+        return num;
+    }
+}
+
+// function makeNegative(num) {
+//     return -Math.abs(num);
+// }
+
+// function makeNegative(num) {
+//     return num < 0 ? num : -num;
+// }
+
+console.log(makeNegative(5));
