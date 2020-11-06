b16edfd20291595ef9423d9da4bcafe0f0b891e1
Tanya Kravchenko <mail-tanja@mail.ru>
2020-11-06 21:20:34 +0300

    Task11_Convert_boolean_values_to_strings_'Yes'_or_'No'

---
+++ Task11_Convert_boolean_values_to_strings_'Yes'_or_'No'.js
@@ -0,0 +1,14 @@
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+// function boolToWord( bool ){
+//     return bool ? 'Yes':'No';
+// }
+
+function boolToWord( bool ){
+    if (bool) {
+        return alert('Yes');
+    } else {
+        return alert('No');
+    }
+}
+boolToWord(true);
