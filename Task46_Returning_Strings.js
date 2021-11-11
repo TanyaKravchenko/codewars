0f2f60f7587ba23af818dcf1cba61fb612670bbd
Tanya Kravchenko <mail-tanja@mail.ru>
2021-11-11 23:12:45 +0300

    Task46_Returning_Strings.js/Task47_Jenny's_secret_message.js - fixed

--- Task46_Returning_Strings.js
+++ Task46_Returning_Strings.js
@@ -1,12 +1,4 @@
 // Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
 
-function greet(name){
-    if(name === "Johnny")
-        return "Hello, my love!";
-    return "Hello, " + name + "!";
-}
-
-// function greet(name){
-//     return "Hello, " + (name == "Johnny" ? "my love" : name) + "!";
-// }
+const greet = name => `Hello, ${name} how are you doing today?`;
 
--- Task47_Jenny's_secret_message.js
+++ Task47_Jenny's_secret_message.js
@@ -2,3 +2,13 @@
 //
 // Can you help her?
 
+function greet(name){
+    if(name === "Johnny")
+        return "Hello, my love!";
+    return "Hello, " + name + "!";
+}
+
+// function greet(name){
+//     return "Hello, " + (name == "Johnny" ? "my love" : name) + "!";
+// }
+
