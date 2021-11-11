f418fc71dabe15f74a31ac06fbe8f5e3b6be8065
Tanya Kravchenko <mail-tanja@mail.ru>
2021-11-11 23:10:50 +0300

    Task47_Jenny's_secret_message.js

--- Task46_Returning_Strings.js
+++ Task46_Returning_Strings.js
@@ -1,4 +1,12 @@
 // Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
 
-const greet = name => `Hello, ${name} how are you doing today?`;
+function greet(name){
+    if(name === "Johnny")
+        return "Hello, my love!";
+    return "Hello, " + name + "!";
+}
+
+// function greet(name){
+//     return "Hello, " + (name == "Johnny" ? "my love" : name) + "!";
+// }
 
