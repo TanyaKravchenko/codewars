af61bb7ce0024409ab15bf24b3ffbd2f4f71af89
Tanya Kravchenko <mail-tanja@mail.ru>
2021-12-08 00:07:07 +0300

    Task71_Find_the_first_non-consecutive_number.js+

--- Task71_Find_the_first_non-consecutive_number.js
+++ Task71_Find_the_first_non-consecutive_number.js
@@ -19,11 +19,9 @@
 // Julia: nothing
 // Nim: none(int) (See options)
 
-function firstNonConsecutive(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i + 1] - arr[i] !== 1) {
-            return arr[i + 1];
-        }
+function firstNonConsecutive (arr) {
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i - 1] + 1 !== arr[i]) return arr[i];
     }
     return null;
 }
