28fcbc39507755961d839d84cd9d0ca2bb998b35
Tanya Kravchenko <mail-tanja@mail.ru>
2022-01-22 00:12:42 +0300

    Task81_Will _there_be_enough_space.js+

--- Task81_Will _there_be_enough_space.js
+++ Task81_Will _there_be_enough_space.js
@@ -13,3 +13,21 @@
 //     cap = 10, on = 5, wait = 5 --> 0 # He can fit all 5 passengers
 // cap = 100, on = 60, wait = 50 --> 10 # He can't fit 10 of the 50 waiting
 
+function enough(cap, on, wait) {
+    return Math.max(wait + on - cap, 0)
+}
+
+console.log(enough(10, 5, 5))
+
+function enough(cap, on, wait) {
+    if (on + wait < cap){
+        return 0;
+    } else {
+        return (on + wait) - cap;
+    }
+}
+
+function enough(cap, on, wait) {
+    return cap - wait - on < 0 ? on + wait - cap : 0;
+}
+
