aa2d71b4b2571c6f65c89389ed4d3ad88eb60d37
Tanya Kravchenko <mail-tanja@mail.ru>
2022-11-19 22:36:06 +0300

    All Star Code Challenge #18

--- Task95-98.js
+++ Task95-98.js
@@ -41,10 +41,28 @@ function getAge(inputString){
 // As usual, your function/method should be pure, i.e. it should not mutate the original string.
 
 String.prototype.toAlternatingCase = function () {
-    debugger
    return this.split('').map(a => a === a.toUpperCase() ? a.toLowerCase() : a.toUpperCase()).join('')
 }
 
 console.log('hello world'.toAlternatingCase())
 
+// 98. All Star Code Challenge #18
 
+// Create a function that accepts 2 string arguments and returns an integer of the count of occurrences the 2nd argument is found in the first one.
+//
+//     If no occurrences can be found, a count of 0 should be returned.
+//
+// ("Hello", "o")  ==>  1
+// ("Hello", "l")  ==>  2
+// ("", "z")       ==>  0
+// Notes:
+//
+//     The first argument can be an empty string
+// The second string argument will always be of length 1
+// FUNDAMENTALSSTRINGS
+
+function strCount(str, letter){
+    return str.split('').filter(a => a === letter).length
+}
+
+console.log(strCount('Hello', 'x'))
