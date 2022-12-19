c83b96cfd4d38079300b597f647d4ad6b49dbf8e
Tanya Kravchenko <mail-tanja@mail.ru>
2022-12-19 22:00:49 +0300

    Sum The Strings

--- Task101-104.js
+++ Task101-104.js
@@ -3,20 +3,13 @@
 //
 // All Star Code Challenge #18
 //
-// Create a function that accepts 2 string arguments and returns an integer of the count of occurrences the 2nd argument is found in the first one.
-//
-//     If no occurrences can be found, a count of 0 should be returned.
-//
-// ("Hello", "o")  ==>  1
-// ("Hello", "l")  ==>  2
-// ("", "z")       ==>  0
-// Notes:
-//
-//     The first argument can be an empty string
-// The second string argument will always be of length 1
+// Create a function that accepts 2 string arguments and returns an integer of the count of occurrences the 2nd
+// argument is found in the first one.  If no occurrences can be found, a count of 0 should be returned.  ("Hello",
+// "o")  ==>  1 ("Hello", "l")  ==>  2 ("", "z")       ==>  0 Notes:  The first argument can be an empty string The
+// second string argument will always be of length 1
 
 function strCount(str, letter) {
-    return str.split('').filter(el =>  el === letter).length
+    return str.split('').filter(el => el === letter).length
 }
 
 // function strCount(str, letter){
@@ -42,16 +35,13 @@ console.log(isPalindrome('HEllE'))
 
 
 // 103.Powers of 2
-// Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
-//
-//     Examples
-// n = 0  ==> [1]        # [2^0]
-// n = 1  ==> [1, 2]     # [2^0, 2^1]
-// n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
+// Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with
+// the exponent ranging from 0 to n ( inclusive ).  Examples n = 0  ==> [1]        # [2^0] n = 1  ==> [1, 2]     #
+// [2^0, 2^1] n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
 
 function powersOfTwo(n) {
     let result = []
-    for(let i = 0; i <= n; i++) {
+    for (let i = 0; i <= n; i++) {
         result.push(2 ** i)
     }
     return result
@@ -76,10 +66,33 @@ console.log(powersOfTwo(4))
 function squareOrSquareRoot(array) {
     return array.map(x => {
         const n = Math.sqrt(x)
-        return Number.isInteger(n) ? n : x*x
+        return Number.isInteger(n) ? n : x * x
     })
 }
 
+
+//104. Sum The Strings
+// Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+//
+// Example: (Input1, Input2 -->Output)
+//
+// "4",  "5" --> "9"
+// "34", "5" --> "39"
+// "", "" --> "0"
+// "2", "" --> "2"
+// "-5", "3" --> "-2"
+// Notes:
+//
+// If either input is an empty string, consider it as zero.
+//
+// Inputs and the expected output will never exceed the signed 32-bit integer limit (2^31 - 1)
+
+function sumStr(a, b) {
+
+    return String(+a + +b)
+}
+
+console.log(sumStr('5', '6'))
 
 
 
