6027912ebb918658954158fdcbe23135fe3221a6
Tanya Kravchenko <mail-tanja@mail.ru>
2023-02-02 15:41:51 +0300

    Student's Final Grade

--- Task105-110.js
+++ Task105-110.js
@@ -53,13 +53,7 @@ function findDifference(a, b) {
   console.log(findDifference([3, 2, 5], [1, 4, 4])) 
 
   
-  export const Title = () => {
-    const array = []
-  
-    return <h1>{array.length && 'The title'}</h1>
-  }
-
-// 108.  Task
+ // 108.  Task
 // Given three integers a ,b ,c, return the largest number obtained after inserting the following operators and brackets: +, *, ()
 // In other words , try every combination of a,b,c with [*+()] , and return the Maximum Obtained (Read the notes for more detail about it)
 // Example
@@ -83,3 +77,36 @@ function findDifference(a, b) {
   function expressionMatter(a, b, c) {
     return Math.max(a+b+c, a*b*c, (a+b)*c, a*(b+c));
   }
+
+  // 109. Student's Final Grade
+
+//   Create a function finalGrade, which calculates the final grade of a student depending on two parameters: a grade for the exam and a number of completed projects.
+
+// This function should take two arguments: exam - grade for exam (from 0 to 100); projects - number of completed projects (from 0 and above);
+
+// This function should return a number (final grade). There are four types of final grades:
+
+// 100, if a grade for the exam is more than 90 or if a number of completed projects more than 10.
+// 90, if a grade for the exam is more than 75 and if a number of completed projects is minimum 5.
+// 75, if a grade for the exam is more than 50 and if a number of completed projects is minimum 2.
+// 0, in other cases
+// Examples(Inputs-->Output):
+
+// 100, 12 --> 100
+// 99, 0 --> 100
+// 10, 15 --> 100
+
+// 85, 5 --> 90
+
+// 55, 3 --> 75
+
+// 55, 0 --> 0
+// 20, 2 --> 0
+
+  function finalGrade (exam, projects) {
+    if(exam > 90 || projects > 10) return 100;
+    if(exam > 75 & projects >= 5) return 90;
+    if(exam > 50 & projects >= 2) return 75;
+    return 0;
+  }
+console.log(finalGrade(10,0))
