5fb15092997a0e74002b2064ccd9fcff1934b55f
Tanya Kravchenko <mail-tanja@mail.ru>
2023-03-17 14:23:52 +0300

    Grasshopper - Messi goals function

--- Task111-120.js
+++ Task111-120.js
@@ -13,3 +13,24 @@
 function testEven(n) {
     return n % 2 === 0 ? true : false
 }
+
+// 112.Messi goals function
+// Messi is a soccer player with goals in three leagues:
+
+// LaLiga
+// Copa del Rey
+// Champions
+// Complete the function to return his total number of goals in all three leagues.
+
+// Note: the input will always be valid.
+
+// For example:
+
+// 5, 10, 2  -->  17
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+  }
+
+
+// const goals = (...a) => a.reduce((s, v) => s + v, 0);  
